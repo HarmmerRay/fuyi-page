405462f7de88680fba39e4d3da358735b77de84c
HammerRay <zhao131804@gmail.com>
2025-02-25 23:20:20 +0800

    #[2025.2.25] 资讯页面基本布局

--- src/views/Community.vue
+++ src/views/Community.vue
@@ -1,7 +1,70 @@
 <template>
   <div class="community">
-    <h2>社区资讯</h2>
+    <div class="search-and-post">
+      <van-search v-model="searchQuery" placeholder="搜索资讯" />
+      <img src="@/assets/publish.png" alt="发布" @click="goToPost" class="publish-icon" />
+    </div>
+    <van-tabs v-model="activeTab" @click="fetchData">
+      <van-tab title="附近"></van-tab>
+    </van-tabs>
+    <div class="news-list">
+      <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+
+      </van-list>
+    </div>
   </div>
 </template>
 
 <script setup></script>
+
+<style scoped>
+.community {
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+}
+
+.search-and-post {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 10px;
+}
+
+.publish-icon {
+  width: 40px; /* 设置图标大小 */
+  height: 40px; /* 设置图标大小 */
+  margin-top: 8px;
+  margin-right: 10px;
+  cursor: pointer; /* 鼠标悬停时显示手形图标 */
+}
+
+.news-list {
+  flex: 1;
+}
+
+.news-item {
+  margin-bottom: 10px;
+}
+
+.text-content {
+  padding: 10px;
+}
+
+.title {
+  font-size: 18px;
+  font-weight: bold;
+}
+
+.summary {
+  font-size: 14px;
+  color: #666;
+}
+
+.date {
+  font-size: 12px;
+  color: #999;
+  margin-top: 5px;
+}
+
+
+</style>
